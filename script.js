da07390a219fd1285a7f2277fa01bc0d1b8163f8
isaack00 <ikat0002@student.monash.edu>
2025-10-13 14:05:01 +1100

    Added the death damage plot

--- script.js
+++ script.js
@@ -3,4 +3,5 @@ vegaEmbed('#barchart_div', 'disaster_freq_barchart.json').catch(console.error);
 vegaEmbed('#symbol_map', 'map.json').catch(console.error);
 vegaEmbed('#line_graph_div', 'affected_time_line.json').catch(console.error);
 
+vegaEmbed('#dot_plot_div', 'death_damage_dot_plot.json').catch(console.error);
 
