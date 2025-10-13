25b93689f3a1f61cca35e7c7b01c7622d7238198
isaack00 <ikat0002@student.monash.edu>
2025-10-13 13:41:48 +1100

    Added the linegrapgh of time vs affected

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 vegaEmbed('#barchart_div', 'disaster_freq_barchart.json').catch(console.error);
 vegaEmbed('#symbol_map', 'map.json').catch(console.error);
+vegaEmbed('#line_graph_div', 'affected_time_line.json').catch(console.error);
 
 
