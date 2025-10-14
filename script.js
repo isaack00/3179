74eb300e07ea5e38865dcdb471b09b614acd1bf4
isaack00 <ikat0002@student.monash.edu>
2025-10-14 16:46:12 +1100

    added the new file and added boxplot and changed dot plot

--- script.js
+++ script.js
@@ -5,11 +5,13 @@ vegaEmbed('#line_graph_div', 'affected_time_line.json').catch(console.error);
 
 vegaEmbed('#dot_plot_div', 'death_damage_dot_plot.json').catch(console.error);
 
-vegaEmbed('#bio_pie', 'bio_pie_chart.json').catch(console.error);
+//vegaEmbed('#bio_pie', 'bio_pie_chart.json').catch(console.error);
 vegaEmbed('#cli_pie', 'clime_pie_chart.json').catch(console.error);
-vegaEmbed('#geo_pie', 'geo_pie_chart.json').catch(console.error);
+//vegaEmbed('#geo_pie', 'geo_pie_chart.json').catch(console.error);
 vegaEmbed('#hyd_pie', 'hydro_pie_chart.json').catch(console.error);
 vegaEmbed('#met_pie', 'Meter_pie_chart.json').catch(console.error);
 
 vegaEmbed('#all_pie', 'all_pie_chart.json').catch(console.error);
+
+vegaEmbed('#boxplot_div', 'boxplot_yearly_occurence.json').catch(console.error);
 
